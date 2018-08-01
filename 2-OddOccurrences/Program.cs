9ee93823b739fa916c9e214c1651a83140ab6dce
Nnavi <nnavi@students.softuni.bg>
2018-08-01 13:38:34 +0300

    Add project files.

---
+++ 2-OddOccurrences/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write a program that extracts from a given sequence of words all elements that presents in it odd number of times (case INsensitive)
+            string [] wordsInput = Console.ReadLine().ToLower().Split();
+            var countElem = new Dictionary<string,int>();
+
+            foreach(var element in wordsInput)
+            {
+                if (!countElem.ContainsKey(element))
+                {
+                    countElem.Add(element, 1);
+                }
+                else
+                {
+                    countElem[element]++;
+                }
+            }
+            List <string> result = new List<string>();
+
+            
+            foreach (var element in countElem)
+            {
+                if (element.Value % 2 != 0)
+                {
+                    result.Add(element.Key);
+                                  
+                }
+            }
+            Console.WriteLine(string.Join(", ",result));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 4-Largest3Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _4_Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read a list of REAL numbers and print LARGEST 3 of them. If less than 3 numbers exit, print all of them.
+            Console.WriteLine(string.Join(" ", 
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .OrderByDescending(x=>x)
+                .Take(3)
+                .ToArray()));
+        }
+        // we make direct print of a concatenated string array made from the input (console) and splitted, parsed to int, ordered by descending, take 3 (largest) and then made in array
+    }
+}
